c71e721ef8a0798f776e2f3839495ca01fab32af
abruno36 <abruno36@gmail.com>
2023-10-28 10:38:30 -0300

    first commit

---
+++ FilmesApi/Data/DTOs/ReadFilmeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FilmesApi.Data.DTOs;
+
+public class ReadFilmeDto
+{
+    public string Titulo { get; set; } = string.Empty;
+    public string Genero { get; set; } = string.Empty;
+    public int Duracao { get; set; } = 0;
+    public DateTime HoraDaConsulta { get; set; } = DateTime.Now;
+}
